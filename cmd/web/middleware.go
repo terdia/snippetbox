6d5478affd9d27ded64760d50a2578d51a2d0a1f
terdia <terrymarcy2000@yahoo.com>
2021-08-14 12:50:34 +0200

    Add middleware
    # recover panoic middleware
    # log all request middleware
    # secure headers middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+		// Any code here will execute on the way down the chain.
+		rw.Header().Set("X-XSS-Protection", "1; mode=block")
+		rw.Header().Set("X-Frame-Options", "deny")
+
+		next.ServeHTTP(rw, r)
+		// Any code here will execute on the way back up the chain.
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		//log every request like so e.g. 172.18.0.1:60504 - HTTP/1.1 GET /snippet?id=4
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(rw, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event
+		// of a panic as Go unwinds the stack).
+		defer func() {
+			// Check if there has been a panic or not use the builtin recover function.
+			if err := recover(); err != nil {
+				// Set a "Connection: close" header on the response,
+				//a triggers an automatic close of current connection.
+				//after a response has been sent
+				rw.Header().Set("Connection", "close")
+
+				app.serverError(rw, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(rw, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -12,5 +20,6 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return mux
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standardMiddleware.Then(mux)
 }
