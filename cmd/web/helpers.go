5ce4351d13bf2d9917e9fb2e927a3ea7927df122
terdia <terrymarcy2000@yahoo.com>
2021-08-15 21:40:10 +0200

    unhandled error ci failing

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -10,7 +10,8 @@ import (
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.logger.Error.Output(2, trace)
+	_ = app.logger.Error.Output(2, trace)
+
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
