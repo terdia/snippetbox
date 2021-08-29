e4eecc34f2b0443cf8ad8c5998d643779477674f
terdia <terrymarcy2000@yahoo.com>
2021-08-29 16:00:22 +0200

    add dynamicMiddlewares type

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,6 +6,8 @@ import (
 	"github.com/go-chi/chi"
 )
 
+type dynamicMiddlewares []func(http.Handler) http.Handler
+
 func (app *application) routes() http.Handler {
 
 	router := chi.NewRouter()
@@ -14,16 +16,22 @@ func (app *application) routes() http.Handler {
 	// which will be used for every request our application receives.
 	router.Use(app.recoverPanic, app.logRequest, secureHeaders)
 
-	router.With(app.session.Enable, noSurf, app.authenticate).Get("/", app.home)
-	router.With(app.session.Enable, noSurf, app.authenticate, app.requireAuthentication).Get("/snippet/create", app.createSnippetForm)
-	router.With(app.session.Enable, noSurf, app.authenticate, app.requireAuthentication).Post("/snippet/create", app.createSnippet)
-	router.With(app.session.Enable, noSurf, app.authenticate).Get("/snippet/{id}", app.showSnippet)
+	dm := dynamicMiddlewares{
+		app.session.Enable,
+		noSurf,
+		app.authenticate,
+	}
 
-	router.With(app.session.Enable, noSurf, app.authenticate).Get("/user/signup", app.signupUserForm)
-	router.With(app.session.Enable, noSurf, app.authenticate).Post("/user/signup", app.signupUser)
-	router.With(app.session.Enable, noSurf, app.authenticate).Get("/user/login", app.loginUserForm)
-	router.With(app.session.Enable, noSurf, app.authenticate).Post("/user/login", app.loginUser)
-	router.With(app.session.Enable, noSurf, app.authenticate, app.requireAuthentication).Post("/user/logout", app.logoutUser)
+	router.With(dm...).Get("/", app.home)
+	router.With(append(dm, app.requireAuthentication)...).Get("/snippet/create", app.createSnippetForm)
+	router.With(append(dm, app.requireAuthentication)...).Post("/snippet/create", app.createSnippet)
+	router.With(dm...).Get("/snippet/{id}", app.showSnippet)
+
+	router.With(dm...).Get("/user/signup", app.signupUserForm)
+	router.With(dm...).Post("/user/signup", app.signupUser)
+	router.With(dm...).Get("/user/login", app.loginUserForm)
+	router.With(dm...).Post("/user/login", app.loginUser)
+	router.With(append(dm, app.requireAuthentication)...).Post("/user/logout", app.logoutUser)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handle("/static/*", http.StripPrefix("/static", fileServer))
